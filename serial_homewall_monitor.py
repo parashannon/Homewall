beaab13c282ca318e5990afb94fb2a076a609e29
parashannon <48845390+parashannon@users.noreply.github.com>
2024-02-02 15:31:03 -0800

    Update serial_homewall_monitor.py

--- serial_homewall_monitor.py
+++ serial_homewall_monitor.py
@@ -18,7 +18,7 @@ def get_serial_port_name():
         lines = dmesg_output.split('\n')
         line=lines[-2]
         print(line)
-        latest_ttyacm=line[45:51]
+        latest_ttyacm=line[35:41]
         print(latest_ttyacm)
     except subprocess.CalledProcessError as e:
         print(f"Error running dmesg: {e}")

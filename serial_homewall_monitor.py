4fa0f8fc6449db4e3d021aa2b44522f4ea9d3590
parashannon <48845390+parashannon@users.noreply.github.com>
2024-02-04 09:43:59 -0800

    Update serial_homewall_monitor.py

--- serial_homewall_monitor.py
+++ serial_homewall_monitor.py
@@ -13,7 +13,6 @@ def get_output_filename():
 def get_serial_port_name():
     latest_ttyacm = None
 
-
     # Run dmesg and filter with grep to get the serial port name attached to 1-1.4
     dmesg_output = subprocess.check_output(['dmesg | grep "USB ACM device"'], shell=True).decode('utf-8')
     lines = dmesg_output.split('\n')
@@ -22,7 +21,7 @@ def get_serial_port_name():
     pattern= re.compile(r'tty([^\s:]+)')
     match = pattern.search(line)
     print(match)
-    latest_ttyacm='/dev/'+match
+    latest_ttyacm='/dev/'+match.group(0)
     print(latest_ttyacm)
 
 
@@ -39,16 +38,7 @@ output_file = get_output_filename()
 
 # Open the output file in append mode
 with open(output_file, 'a') as file:
-    # Run dmesg and filter with grep to get the serial port name attached to 1-1.4
-    dmesg_output = subprocess.check_output(['dmesg | grep "USB ACM device"'], shell=True).decode('utf-8')
-    lines = dmesg_output.split('\n')
-    line=lines[-2]
-    print(line)
-    pattern= re.compile(r'tty([^\s:]+)')
-    match = pattern.search(line)
-    print(match)
-    latest_ttyacm='/dev/'+match.group(0)
-    print(latest_ttyacm)
+
 
     try:
         while True:
